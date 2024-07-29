a0686828400fabd2d6689a9e1ded29b889fbe26a
pandarudra <rudrapanda8206@gmail.com>
2024-07-29 22:34:55 +0530

    wp support

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests as req
 import os
-from plyer import notification
+import pywhatkit as pykt
+# from plyer import notification
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -34,21 +35,28 @@ def rain_check(data):
             return True
     return False
 
-def notifier():
-    notification.notify(
-        title="Rain Alert!",
-        message="It's going to rain today!",
-        app_name="Rain Alert",
-        timeout=10, 
-    )
+def notifier(number,message,hour,minute):
+    pykt.sendwhatmsg(number,message,hour,minute)
+    
 def main():
     ip=getip()
     LOC=getloc(ip)
     
     city_data=get_weather(LOC,KEY)
+    print("Please enter the  hour and minute in 24 hour format E.g. 13:30 for 1:30 PM")
+    number=input("Enter the number:")
+    message=input("Enter the message:")
+    hour=int(input("Enter the hour:"))
+    minute=int(input("Enter the minute:"))
+    
     if rain_check(city_data):
-        notifier()
-    print(LOC)
+        notifier(
+            number,
+            message+". It's going to rain today at "+LOC+" in few hours",
+            hour,
+            minute
+        )
+    
 
 
 
